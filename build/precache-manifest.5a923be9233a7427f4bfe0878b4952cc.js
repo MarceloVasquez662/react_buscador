6d66254302af284614c192e469105bd0455cd517
MarceloVasquez662 <marcelovasquez662@gmail.com>
2022-02-11 12:13:03 -0300

    Deploy

---
+++ build/precache-manifest.5a923be9233a7427f4bfe0878b4952cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9987b40148756d2bc882339486216a57",
+    "url": "/react_buscador/index.html"
+  },
+  {
+    "revision": "79e80e131081524fc961",
+    "url": "/react_buscador/static/css/main.57e598a7.chunk.css"
+  },
+  {
+    "revision": "35b3d7f1b552c81219b7",
+    "url": "/react_buscador/static/js/2.ee574aec.chunk.js"
+  },
+  {
+    "revision": "79e80e131081524fc961",
+    "url": "/react_buscador/static/js/main.e4ea7e4a.chunk.js"
+  },
+  {
+    "revision": "aa538897ca879acc9cf6",
+    "url": "/react_buscador/static/js/runtime-main.265d96fb.js"
+  },
+  {
+    "revision": "021b9945a4e4e39ac7e7e6b95afc573b",
+    "url": "/react_buscador/static/media/logo.021b9945.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React_Buscador/precache-manifest.77c5d4dae882eea3a4a7f001610dd05f.js"
+  "/react_buscador/precache-manifest.5a923be9233a7427f4bfe0878b4952cc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_Buscador/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_buscador/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
